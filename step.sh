7cfa5a695c60fa144144fc298a3568cc316b5a5c
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-03-09 10:39:17 +0100

    Add step sh

---
+++ step.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+ENTRY_FILE="./index.${platform}.js"
+BUNDLE_OUTPUT="${platform}/main.jsbundle"
+
+MINIFY=""
+ROOT=""
+ASSETROOTS=""
+OUT=""
+URL=""
+
+# Input params
+
+if [[ "${minify}" == "true" ]] ; then
+	MINIFY='--minify'
+fi
+
+if [ ! -z "${root}" ] ; then
+    ROOT="--root ${root}"
+fi
+
+if [ ! -z "${assetRoots}" ] ; then
+    ASSETROOTS="--assetRoots ${assetRoots}"
+fi
+
+if [ ! -z "${out}" ] ; then
+    OUT="--out ${out}"
+fi
+
+if [ ! -z "${url}" ] ; then
+    URL="--url ${url}"
+fi
+
+# Bundle
+
+react-native bundle --entry-file "$ENTRY_FILE" --platform ${platform} --bundle-output "$BUNDLE_OUTPUT" \
+  "--dev ${dev}" \
+  ${MINIFY} \
+  ${ROOT} \
+  ${ASSETROOTS} \
+  ${OUT} \
+  ${URL}
